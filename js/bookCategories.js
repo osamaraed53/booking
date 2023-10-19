5357cb00e4c2b71c01aed2fe3aad2944b5885284
osamaraed53 <osamaraed53@gmail.com>
2023-10-19 06:26:09 +0300

    new

--- js/bookCategories.js
+++ js/bookCategories.js
@@ -3,18 +3,19 @@ let selectedGenre = "none";
 let selectedAgeGroup = "none";
 let selectedLanguage = "English";
 
-// Initial display Pagination 
+// Initial display Pagination
 let currentPage = 1;
 const itemsPerPage = 10;
 //  ------------------------- createCard ------------------------
 function createCard(data) {
   const urlSearchParams = new URLSearchParams();
-  urlSearchParams.append('id', data["id"]);
-  const href = 'http://127.0.0.1:5500/html/Detail.html?' + urlSearchParams.toString();
+  urlSearchParams.append("id", data["id"]);
+  const href =
+    "http://127.0.0.1:5502/html/Detail.html?" + urlSearchParams.toString();
 
   var newBooks = document.getElementById("newBooks-section");
-  var newBooks_card = document.createElement('div');
-  newBooks_card.classList.add('book-card');
+  var newBooks_card = document.createElement("div");
+  newBooks_card.classList.add("book-card");
 
   newBooks_card.innerHTML = `
       <a href="${href}"> 
@@ -26,32 +27,29 @@ function createCard(data) {
   newBooks.appendChild(newBooks_card);
 }
 
-
 // ------------------------------ All search function ---------------------------
-async function getDataFromJsonById(id = '') {
+async function getDataFromJsonById(id = "") {
   const response = await fetch(`http://localhost:3000/books/${id}`);
   const data = await response.json();
   return data;
 }
 
-async function getDataFromJsonBySearchQuery(searchQuery){
+async function getDataFromJsonBySearchQuery(searchQuery) {
   const response = await fetch(`http://localhost:3000/books?q=${searchQuery}`);
   const data = await response.json();
   return data;
-
 }
-
 
 // ------------------------------------- all function about Pagination ---------------------------------
 function updatePagination(totalPages) {
   let pagination = document.getElementById("pagination");
-  pagination.innerHTML = '';
+  pagination.innerHTML = "";
 
   for (let i = 1; i <= totalPages; i++) {
-    let pageLink = document.createElement('a');
-    pageLink.href = '#';
+    let pageLink = document.createElement("a");
+    pageLink.href = "#";
     pageLink.textContent = i;
-    pageLink.addEventListener('click', (e) => {
+    pageLink.addEventListener("click", (e) => {
       e.preventDefault();
       currentPage = i;
       work();
@@ -60,101 +58,97 @@ function updatePagination(totalPages) {
   }
 }
 
-
-async function work(ifQuery="undefined") {
+async function work(ifQuery = "undefined") {
   let location = document.getElementById("newBooks-section");
-  location.innerHTML = ' ';
+  location.innerHTML = " ";
   let dataArray = [];
-  if(ifQuery != "undefined"){
-    dataArray = await getDataFromJsonBySearchQuery(ifQuery);}
-  else  {
+  if (ifQuery != "undefined") {
+    dataArray = await getDataFromJsonBySearchQuery(ifQuery);
+  } else {
     // Fetch data
     for (let i = 1; i <= 82; i++) {
       const data = await getDataFromJsonById(i);
-      const checker  = displayDataAfterFilter(data)
-      if (checker== undefined){
-        continue
-      }else
-      {
+      const checker = displayDataAfterFilter(data);
+      if (checker == undefined) {
+        continue;
+      } else {
         dataArray.push(checker);
       }
     }
-}
+  }
 
-for (let i = (currentPage - 1) * itemsPerPage; i <  currentPage * itemsPerPage && i < dataArray.length; i++) {
-      createCard(dataArray[i]);
+  for (
+    let i = (currentPage - 1) * itemsPerPage;
+    i < currentPage * itemsPerPage && i < dataArray.length;
+    i++
+  ) {
+    createCard(dataArray[i]);
   }
 
   // Update pagination
   const totalPages = Math.ceil(dataArray.length / itemsPerPage);
   updatePagination(totalPages);
-
-
 }
 
-
-// --------------------Filter data based on Categories user and EventListener for it --------------------------------- 
+// --------------------Filter data based on Categories user and EventListener for it ---------------------------------
 function displayDataAfterFilter(array) {
-   if(selectedAgeGroup == "none" && selectedGenre == "none" && selectedLanguage== "English"){
-      // console.log(array["categories"]);
-      return array;
-    }
-    if(array["categories"] == selectedLanguage){
-      // console.log(array);
-      return array;
-     }  
-      if(array["categories"] == selectedGenre){
-      // console.log(selectedGenre);
-      // console.log(array["categories"]);
-      return array;
-    }
-    if(array["categories"] == selectedAgeGroup){
-      // console.log(array);
-      return array;
-    }   
+  if (
+    selectedAgeGroup == "none" &&
+    selectedGenre == "none" &&
+    selectedLanguage == "English"
+  ) {
+    // console.log(array["categories"]);
+    return array;
+  }
+  if (array["categories"] == selectedLanguage) {
+    // console.log(array);
+    return array;
+  }
+  if (array["categories"] == selectedGenre) {
+    // console.log(selectedGenre);
+    // console.log(array["categories"]);
+    return array;
+  }
+  if (array["categories"] == selectedAgeGroup) {
+    // console.log(array);
+    return array;
+  }
 }
 
-
-// if click in button will get data from input and 
-document.getElementById('buttonOfSearchByquery').addEventListener('click', function(event) {
-  event.preventDefault(); // Prevent the form from being submitted
-  searchByQuery();
-});
+// if click in button will get data from input and
+document
+  .getElementById("buttonOfSearchByquery")
+  .addEventListener("click", function (event) {
+    event.preventDefault(); // Prevent the form from being submitted
+    searchByQuery();
+  });
 
 function searchByQuery() {
-  console.log("osama")
-  let searchQuery = document.getElementById('searchByquery').value;
+  console.log("osama");
+  let searchQuery = document.getElementById("searchByquery").value;
   work(searchQuery);
-  document.getElementById('searchByquery').value = '';
+  document.getElementById("searchByquery").value = "";
 }
 
-
-const ageGroupSelect = document.getElementById('ageGroup');
+const ageGroupSelect = document.getElementById("ageGroup");
 ageGroupSelect.addEventListener("change", (e) => {
   selectedAgeGroup = e.target.value;
   currentPage = 1;
   work();
 });
 
-const languageSelect = document.getElementById('language');
+const languageSelect = document.getElementById("language");
 languageSelect.addEventListener("change", (e) => {
   selectedLanguage = e.target.value;
   currentPage = 1;
   work();
 });
 
-
-const GenreSelect = document.getElementById('genre');
+const GenreSelect = document.getElementById("genre");
 GenreSelect.addEventListener("change", (e) => {
   selectedGenre = e.target.value;
   currentPage = 1;
   work();
 });
 
-
-
-
 work();
-
-
-
--- js/profile-page.js
+++ js/profile-page.js
@@ -1,28 +1,105 @@
+// document.addEventListener("DOMContentLoaded", function () {
+//     // قم بجلب البيانات من ملف JSON (تأكد من تعديل المسار إلى ملف JSON الخاص بك)
+//     fetch('http://localhost:3000/User')
+//         .then(response => response.json())
+//         .then(data => {
+//             // يمكنك تعديل هذا الجزء بناءً على هيكل بيانات الملف JSON
+//             const userId = 1; // قم بتعيين معرف المستخدم الحالي
+
+//             // ابحث عن المستخدم بناءً على المعرف
+//             const user = data.User.find(user => user.id === userId);
+
+//             if (user) {
+//                 // عرض بيانات المستخدم على الصفحة
+//                 document.getElementById('user-name').textContent = user.Username;
+//                 document.getElementById('email').textContent = user.email;
+//                 // يمكنك استمرار عرض أي بيانات إضافية هنا
+
+//                 // إظهار إعلان بأن البيانات تم استرجاعها بنجاح
+//                 alert('تم استرجاع البيانات بنجاح!');
+//             } else {
+//                 // إذا لم يتم العثور على المستخدم
+//                 alert('المستخدم غير موجود في قاعدة البيانات.');
+//             }
+//         })
+        
+//         .catch(error => {
+//             // إذا كان هناك خطأ في جلب البيانات
+//             console.error('حدث خطأ أثناء جلب البيانات: ', error);
+//             alert('حدث خطأ أثناء جلب البيانات. الرجاء التحقق من الاتصال بالخادم.');
+//         });
+// });
 
 
+
+
+// document.getElementById('user-name').textContent = user.Username;
+// document.getElementById('email').textContent = user.email;
+
+
+
+
+
+
+
+
+
+
+
+
+// ****************************************************************************
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+    // استخراج البيانات من Local Storage
+    // const storedUsername = localStorage.getItem("username");
+    // const storedEmail = localStorage.getItem("email");
+
+    // عرض البيانات في الصفحة
+//     const usernameElement = document.getElementById("user-name");
+//     const emailElement = document.getElementById("email");
+
+//     usernameElement.textContent = storedUsername;
+//     emailElement.textContent = storedEmail;
+// });
+// // تحميل البيانات من ملف JSON عند تحميل الصفحة
+// document.addEventListener("DOMContentLoaded", function () {
+//     fetch("data.json") // استبدل بالمسار الصحيح لملف JSON
+//         .then((response) => response.json())
+//         .then((jsonData) => {
+//             // عرض البيانات في صفحة البروفايل
+//             const usernameElement = document.getElementById("user-name");
+//             const emailElement = document.getElementById("email");
+//             usernameElement.textContent = jsonData.username;
+//             emailElement.textContent = jsonData.email;
+//         })
+//         .catch((error) => {
+//             console.error("حدث خطأ: " + error);
+//         });
+// });
+
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     // استخراج البيانات من Local Storage
+//     const storedUsername = localStorage.getItem("username");
+//     const storedEmail = localStorage.getItem("email");
+
+//     // عرض البيانات في الصفحة
+//     const usernameElement = document.getElementById("user-name");
+//     const emailElement = document.getElementById("email");
+
+//     usernameElement.textContent = storedUsername;
+//     emailElement.textContent = storedEmail;
+// });
 document.addEventListener("DOMContentLoaded", function () {
-    const userNameElement = document.getElementById("user-name");
-    const userEmailElement = document.getElementById("email");
-  
-    // استرجاع بيانات المستخدم من Session Storage
-    const storedUsername = sessionStorage.getItem("username");
-    const storedEmail = sessionStorage.getItem("email");
-  
-    // عرض بيانات المستخدم إذا تم تخزينهما في Session Storage
-    if (storedUsername) {
-      userNameElement.textContent = storedUsername;
-    }
-    if (storedEmail) {
-      userEmailElement.textContent = storedEmail;
-    }
-  });
-  
-  document.addEventListener("DOMContentLoaded", function () {
-    const userName = sessionStorage.getItem("username");
-    const userEmail = sessionStorage.getItem("email");
-  
-    // عرض البيانات في صفحة البروفايل
-    document.getElementById("user-name").textContent = userName;
-    document.getElementById("email").textContent = userEmail;
-  });
-  
+    // استخراج البيانات من Local Storage
+    const storedUsername = localStorage.getItem("Username");
+    const storedEmail = localStorage.getItem("email");
+
+    // عرض البيانات في الصفحة
+    const usernameElement = document.getElementById("user-name");
+    const emailElement = document.getElementById("email");
+
+    usernameElement.textContent = storedUsername;
+    emailElement.textContent = storedEmail;
+});
